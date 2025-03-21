c47bdaf5b630769938d714ff121c7cdee507a1da
Toomore Chiang <toomore0929@gmail.com>
2025-03-21 16:47:10 +0800

    Add relay's version, asn in api
    Signed-off-by: Toomore Chiang <toomore0929@gmail.com>

--- pulse/backend/api.py
+++ pulse/backend/api.py
@@ -1,6 +1,7 @@
 ''' API '''
 from fastapi import FastAPI
 from routers import vega
+from fastapi.middleware.cors import CORSMiddleware
 
 TAG_META = [
     {'name': 'vega', 'description': 'For Vega-Lite output graphics.'}
@@ -22,7 +23,18 @@ app = FastAPI(
         'url': 'https://github.com/ocftw/ooni-research/blob/main/asn_coverage/LICENSE',
     }
 )
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://127.0.0.1:8000", ],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
 app.include_router(vega.router)
+
 
 @app.get('/')
 async def main():
--- pulse/backend/routers/vega.py
+++ pulse/backend/routers/vega.py
@@ -1,7 +1,7 @@
 """ Route Vega-Lite """
 from fastapi import APIRouter, status
 from pgdb import PGConn
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 from datetime import datetime
 from enum import Enum
 
@@ -14,20 +14,26 @@ router = APIRouter(
 class Country(Enum):
     tw = 'tw'
 
-class Relays(BaseModel):
+class RelaysRunning(BaseModel):
     created_at: datetime
     count: int
-    running: bool
-    observed_bandwidth: float
+    running: bool = Field(default=False)
+    observed_bandwidth: float = Field(default=0)
 
-class ResponseRelays(BaseModel):
-    country: Country
-    result: list[Relays]
+class RelaysVersion(BaseModel):
+    created_at: datetime
+    count: int
+    version: str = Field(default="")
+
+class RelaysASN(BaseModel):
+    created_at: datetime
+    count: int
+    asn: str
 
 
-@router.get('/tor_relays')
-async def tor_relays(country: Country) -> ResponseRelays:
-    ''' Get Tor Relays by country'''
+@router.get('/tor/relays/running')
+async def tor_relays_running(country: Country) -> list[RelaysRunning]:
+    ''' Get Tor Relays Running by country'''
     datas = []
     with PGConn() as pg_conn:
         for row in pg_conn.cur.execute('''select date(created_at) as dt,
@@ -35,9 +41,46 @@ async def tor_relays(country: Country) -> ResponseRelays:
                                                  running,
                                                  round(sum(observed_bandwidth)/count(date(created_at))*count(DISTINCT fingerprint), 4)
                                        from relay_details
+                                       where country=%s
                                        group by dt, running
                                        order by dt, running
-                                      ;'''):
-            datas.append(Relays(created_at=row[0], count=row[1], running=row[2], observed_bandwidth=row[3]))
-    
-    return ResponseRelays(country=country, result=datas)
+                                      ;''', (country, )):
+            datas.append(RelaysRunning(created_at=row[0], count=row[1], running=row[2], observed_bandwidth=row[3]))
+
+    return datas
+
+@router.get('/tor/relays/version')
+async def tor_relays_version(country: Country) -> list[RelaysVersion]:
+    '''Get Tor Relays Version by country'''
+    datas = []
+    with PGConn() as pg_conn:
+        for row in pg_conn.cur.execute('''
+                                       select date(created_at) as dt,
+                                              count(DISTINCT fingerprint),
+                                              version
+                                       from relay_details
+                                       where country=%s
+                                       group by dt, version
+                                       order by dt, version desc;
+                                      ;''', (country, )):
+            datas.append(RelaysVersion(created_at=row[0], count=row[1], version=row[2]))
+
+    return datas
+
+@router.get('/tor/relays/asn')
+async def tor_relays_asn(country: Country) -> list[RelaysASN]:
+    '''Get Tor Relays Version by country'''
+    datas = []
+    with PGConn() as pg_conn:
+        for row in pg_conn.cur.execute('''
+                                       select date(created_at) as dt,
+                                              count(DISTINCT fingerprint),
+                                              asn
+                                       from relay_details
+                                       where country=%s
+                                       group by dt, asn
+                                       order by dt, asn;
+                                      ;''', (country, )):
+            datas.append(RelaysASN(created_at=row[0], count=row[1], asn=row[2]))
+
+    return datas
