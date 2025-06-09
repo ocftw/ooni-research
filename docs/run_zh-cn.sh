7bfa216bb10a9fb750a3bb0d0f8f20a01266847b
Toomore Chiang <toomore0929@gmail.com>
2025-06-09 18:46:12 +0800

    Add about us
    Signed-off-by: Toomore Chiang <toomore0929@gmail.com>

--- docs/run_zh-cn.sh
+++ docs/run_zh-cn.sh
@@ -4,6 +4,7 @@ export SITE_NAME='OONI-Research 网络干预开放观察计划'
 export SITE_URL='https://ooni-research.ocf.tw/docs/zh-cn/'
 export EDIT_URI='https://github.com/ocftw/ooni-research/blob/main/docs/zh-CN/'
 export SITE_DESC='匿名网络、Tor、Tails、OONI、网络自由、网络审查、ASNs 观测范围、检测列表、本地推广与翻译 '
+export NAV_ABOUT='关于我们'
 export NAV_INTRO='项目简介'
 export NAV_EVENT='活动参与'
 export NAV_PP='参与项目'
@@ -15,5 +16,4 @@ export CATE_NAME='文章类型'
 export LANGUAGE='zh'
 export OVERRIDES='overrides_cn'
 
-#mkdocs build -s -f ./mkdocs_cn.yml -d ./output/zh-cn
-mkdocs serve -v -f ./mkdocs_cn.yml
+mkdocs build -s -f ./mkdocs_cn.yml -d ./output/zh-cn
